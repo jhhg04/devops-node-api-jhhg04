623071c5446a028ec6e2ce747f00c1727304383c
jhhg820 <jhhg04@gmail.com>
2026-05-06 19:27:48 -0500

    update PORT var

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log(`Server running on port ${PORT}`);
 });
